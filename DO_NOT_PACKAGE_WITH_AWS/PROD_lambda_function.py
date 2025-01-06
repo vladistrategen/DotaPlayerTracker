32636c8498141ed0b1b263b45856dc6a1bf06101
Vlad Istrate <vlad.istrate02@gmail.com>
2025-01-06 18:54:03 +0200

    update messages

--- DO_NOT_PACKAGE_WITH_AWS/PROD_lambda_function.py
+++ DO_NOT_PACKAGE_WITH_AWS/PROD_lambda_function.py
@@ -1,3 +1,4 @@
+import random
 import requests
 import os
 from datetime import datetime
@@ -11,6 +12,9 @@ PLAYER_NAME = os.environ.get('PLAYER_NAME')  # Player name for getting the leade
 TEAM_ID = os.environ.get('TEAM_ID')  # Team ID for getting the leaderboard rank
 TEAM_TAG = os.environ.get('TEAM_TAG')  # Team tag for getting the leaderboard rank
 COUNTRY_CODE = os.environ.get('COUNTRY_CODE')  # Country code for getting the leaderboard rank
+
+positive_emojis = ["🏆", "👑", "💰", "🪙", "💵", "👙", "🤤", "🔥", "💯", "👆"]
+negative_emojis = ["🚫🏠", "😔", "💐","🪦", "💀", "💩"]
 
 # Headers for Discord API requests using the bot token
 HEADERS = {
@@ -74,13 +78,14 @@ def get_channel_message_and_name(leaderboard_rank: int, old_channel_name: str):
         if leaderboard_rank < last_rank:
             message += old_rank_message
             message += new_rank_message
-            message += " 🏆 👑 💰 💰 🪙 💵 👙 🤤 🔥 💯 👆"
+            # add a random number of positive emojis
+            message += " " + " ".join(random.choices(positive_emojis, k=5))
             channel_name+=' 📈'
         elif leaderboard_rank > last_rank:
             message += old_rank_message
             message += new_rank_message
             channel_name+=' 📉'
-            message += " 🚫🏠😔 🚫🏠😔 💐💐"
+            message += " " + " ".join(random.choices(negative_emojis, k=5))
         else:
             message += ", este tot pe locul **"+str(leaderboard_rank)+"**"
             message += " :neutral_face: :neutral_face: :neutral_face:"
@@ -88,56 +93,72 @@ def get_channel_message_and_name(leaderboard_rank: int, old_channel_name: str):
     else:
         message += new_rank_message
 
-
     message+="\n"
 
     if leaderboard_rank <= 10:
         message += "Ai terminat DOTA, bravo!"
     elif leaderboard_rank <= 50:
-        message += "Bravo, esti un zeu"
-    elif leaderboard_rank <= 70:
+        message += "THE GOD OF DOTA"
+    elif leaderboard_rank <= 75:
         message += "Tatal tau este mandru de tine!"
     elif leaderboard_rank <= 100:
         message += "He is locked in, he takes no prisoners, there is no one to tell the tale..."
     elif leaderboard_rank <= 125:
-        message += "Inca putin si vine salariul"
+        message += '⚡⚡⚡⚡\"I AM THE ONE WHO KNOCKS\"⚡⚡⚡⚡'
     elif leaderboard_rank <= 150:
-        message += "Ai scapat de flori, you live to fight another day"
+        message += "HE IS LOCKED THE FUCK IN"
+    elif leaderboard_rank <=175:
+        message += "ARMED AND DANGEROUS! AGAIN!! AGAIN!!! AGAIN!!!! 🔫🔫🔫"
     elif leaderboard_rank <= 200:
-        message += "Jon Snow a venit din morti, tu ai venit din top 200"
+        message += "HELL UNLEASHED!!!!"
+    elif leaderboard_rank <= 225:
+        message += "Time is a flat circle and you are on top of it🔥🔥🔥"
     elif leaderboard_rank <= 250:
-        message += "Inca putin si te lasi de facultate"
+        message += "KING VON N-A MURIT 🔫🔫🔫🔫🔫🔫"
+    elif leaderboard_rank <= 275:
+        message += "BAI NENE PUMNU MEU BETON ARMAT ZICI CA-I TORPILA DE TANC"
     elif leaderboard_rank <= 300:
-        message += "Este doar un mental block... pot intra in top 200"
+        message += "CE LE FACI COPILEEEEEEEEEEEEEEEEEEE"
+    elif leaderboard_rank <= 325:
+        message += "CAN ANYBODY STOP THIS OUTRAGEOUS MAN???!!!!"
     elif leaderboard_rank <= 350:
-        message += "Inca 300 de rankuri si o s-o ai pe Aviana"
-    elif leaderboard_rank < 400:
-        message += "Mda, nu mai esti un gunoi"
-    elif leaderboard_rank < 450:
-        message += "HAAAI ANDREI CA NE AI BAGAT PE TOTI IN SCAUNEEEEEEE"
-    elif leaderboard_rank < 500:
-        message += "The shadow of a once mighty player..."
-    elif leaderboard_rank < 550:
-        message += "For the watch"
-    elif leaderboard_rank < 600:
-        message += "Ce faci, esti prost?"
-    elif leaderboard_rank < 650:
-        message += "Nici gagica n-ai, nici rank n-ai, ce faci cu viata ta?"
-    elif leaderboard_rank < 700:
-        message += "Esti prost, nu te mai uita la monitor"
-    elif leaderboard_rank < 750:
-        message += "Sa speram ca nu cade mai jos de atat..."
-    elif leaderboard_rank < 800:
-        message += "Cum s-a ajuns aici?"
-    elif leaderboard_rank < 900:
-        message += "Esti un GUNOI!!!! Mihai juca mai bine, apuca te de league si joaca Garen ca atata stii sa faci"
-    elif leaderboard_rank < 1000:
+        message += "Hai andreie da in pula mea drumu la brate ce cacat faci?!!!"
+    elif leaderboard_rank <= 375:
+       message += "Coaie adormim aici 🛌" 
+    elif leaderboard_rank <= 400:
+        message += "Pentru asta n ai vrut sa mergi la sala? Pentru asta n ai vrut sa mergem la cabane?"
+    elif leaderboard_rank <= 450:
+        message += "HAAAI COAEEE CA NE AI BAGAT PE TOTI IN SCAUNEEEEEEE"
+    elif leaderboard_rank <= 500:
+        message += "Game of thrones Season 8, Mihai dupa 2020, Andrei dupa liceu"
+    elif leaderboard_rank <= 550:
+        message += "At this point lasa-l pe gabi sa joace in locu tau"
+    elif leaderboard_rank <= 600:
+        message += "Sa-i fie tarana usoara💀💀💀💀"
+    elif leaderboard_rank <= 625:
+        message += "BA ESTI PROST, CE FACI CU VIATA TA?"
+    elif leaderboard_rank <= 650:
+        message += "Nici gagica n-ai, nici rank n-ai, ce faci cu viata ta?💩💩💩💩💩"
+    elif leaderboard_rank <= 700:
+        message += "Stick to replays lil bro, you're not ready for the big leagues"
+    elif leaderboard_rank <= 725:
+        message += "Vai de curu GENETIC FAILURE"
+    elif leaderboard_rank <= 750:
+        message += "👩‍🦽👩‍🦽‍➡️♿👩‍🦽👩‍🦽‍➡️♿👩‍🦽👩‍🦽‍➡️♿"
+    elif leaderboard_rank <= 775:
+        message += "BAAAAAAA, CE PULA MEA FACI?"
+    elif leaderboard_rank <= 800:
+        message += "Mihai a avut dreptate, RANK 53172685378216538712 🗿🗿🗿🗿🗿"
+    elif leaderboard_rank <= 850:
+        message += "You need to make a deal with the DEVIL to get out of this one 🤝🤝🤝🤝🤝 😈🔞"
+    elif leaderboard_rank <= 900:
+        message += "Esti un GUNOI!!!! Mihai juca mai bine, apuca te de LOL si joaca Garen ca atata stii sa faci"
+    elif leaderboard_rank <= 1000:
         message += "Get a job lil bro... https://www.linkedin.com/ "
-    elif leaderboard_rank < 2000:
+    elif leaderboard_rank <= 2000:
         message += "ROMPREST!!!! https://romprest.eu/ "
     else:
-        message += "Esti un gunoi, nu mai juca DOTA"
-
+        message += "Esti un gunoi bun de nimic, da-i uninstall"
     return message, channel_name
 
 def lambda_handler(event, context):
